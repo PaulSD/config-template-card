0f07e580d8ef5110611dfccefa211b8fb61e0d62
Paul Donohue <git@PaulSD.com>
2025-02-17 22:53:56 -0500

    Add support for "static" variables
    These will be evaluated only on the first update/render, and will be
    preserved without re-evaluation for subsequent updates.
    
    These may be used to manage the performance impact of expensive
    operations, by eliminating unnecessary re-evaluations.

--- src/types.ts
+++ src/types.ts
@@ -2,24 +2,29 @@ import { LovelaceCardConfig, EntitiesCardEntityConfig, LovelaceElementConfigBase
 import { HomeAssistant, CurrentUser } from 'custom-card-helpers';
 import { HassEntities } from 'home-assistant-js-websocket';
 
+export type Vars = Record<string, any> | any[];
+
 interface StyleMixin {
   style?: Record<string, string>;
 }
 
-export interface ConfigTemplateConfig {
+export interface Config {
   type: string;
   entities?: string[];
-  variables?: Record<string, any> | any[];
+  variables?: Vars;
+  staticVariables?: Vars;
   card?: LovelaceCardConfig & StyleMixin;
   row?: EntitiesCardEntityConfig & StyleMixin;
   element?: LovelaceElementConfigBase & StyleMixin;
   style?: Record<string, string>;
 }
 
-export interface ConfigTemplateVarMgr {
+export interface VarMgr {
   hass?: HomeAssistant;
   states?: HassEntities;
   user?: CurrentUser;
-  vars?: Record<string, any> & any[];
+  vars?: Vars;
   _evalInitVars?: string;
+  svars?: Vars;
+  _evalInitSVars?: string;
 }
