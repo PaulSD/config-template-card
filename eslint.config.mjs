f750fc8b466e9eb00d737e4636e510f0fc913194
Paul Donohue <git@PaulSD.com>
2025-02-22 12:10:59 -0500

    Cleanup, document initialization, and prep for async changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ export default ts.config(
       '@typescript-eslint/no-unused-vars': ['warn', {'argsIgnorePattern': '^_'}],
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
     },
     languageOptions: {
